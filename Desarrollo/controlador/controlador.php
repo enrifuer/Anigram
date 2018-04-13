0427655d2c357267b1c280f365796a0098c6d3b6
Enrique <enrifuer@ucm.es>
2018-04-13 20:23:54 +0200

    Arreglando sintaxis

--- Desarrollo/controlador/controlador.php
+++ Desarrollo/controlador/controlador.php
@@ -10,8 +10,8 @@ public function __Construct()
 
 
 public function get_user_login($Nombre, $Clave) {
-$user = new Usuario($Nombre, $Clave);
-$datos=$user->buscarUsuarios_login($Nombre, $Clave);
+    $user = new Usuario($Nombre, $Clave);
+    $datos=$user->buscarUsuarios_login($Nombre, $Clave);
 return $datos;
  }
 
